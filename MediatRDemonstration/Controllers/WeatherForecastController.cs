7568c35abe5b58c82a7ffbb35d0810b8e1683b46
Amarnath, Bharath <bharath9788@gmail.com>
2020-08-02 22:49:33 +0530

    Add project files.

---
+++ MediatRDemonstration/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using MediatRDemonstration.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace MediatRDemonstration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IMediator _mediatR;
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediatR)
+        {
+            _logger = logger;
+            _mediatR = mediatR;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{car}")]
+        public Task<IEnumerable<Employee>> Get(string car)
+        {
+           return  _mediatR.Send(new GetAllEmployeesQueries());
+        }
+    }
+}
---
+++ MediatRDemonstration/Services/Queries/GetAllEmployeesQueries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatRDemonstration.Models;
+using System.Threading;
+
+namespace MediatRDemonstration
+{
+    public class GetAllEmployeesQueries :IRequest<IEnumerable<Employee>>
+    {
+
+
+    }
+
+    public class GetAllEmployeesQueriesHandler : IRequestHandler<GetAllEmployeesQueries, IEnumerable<Employee>>
+    {
+        public async Task<IEnumerable<Employee>> Handle(GetAllEmployeesQueries request, CancellationToken cancellationToken)
+        {
+            return new[]
+            {
+                new Employee { firstName = "Bharath", id = 1, lastName = "kumar"},
+                new Employee { firstName = "Bharath_2", id = 3, lastName = "kumar_2"}
+             };
+
+        }
+    }
+}
